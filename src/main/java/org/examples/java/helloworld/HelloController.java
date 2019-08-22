20343349513301468842d7351f0d464e8ee7b996
zerobig <zerobig.kim@gmail.com>
2019-08-22 12:07:20 +0000

    Rollback Hello message

--- src/main/java/org/examples/java/helloworld/HelloController.java
+++ src/main/java/org/examples/java/helloworld/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello Jenkins + Spring Boot!";
+        return "Hello Jenkins X + Spring Boot!";
     }
     
 }
